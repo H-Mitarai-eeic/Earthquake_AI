077ba86c19a93a87718eca89798248ef3aefbc8a
kakeru macbook <kakekakemiya920@g.ecc.u-tokyo.ac.jp>
2021-11-12 03:31:16 +0900

    js needs server

---
+++ UI/js/tmp/receive.py
@@ -0,0 +1,11 @@
+#!/opt/anaconda3/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+
+# recieve = sys.stdin.readline()
+recieve = "hello"
+recieve = recieve + "OK!"
+
+# print('Content-type: text/html\n')
+print(recieve)
