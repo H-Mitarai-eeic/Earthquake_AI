dfbea03caad6d09b6b3ce16ec5dde59331342fc9
H-Mitarai-eeic <mitarai-haruki520@g.ecc.u-tokyo.ac.jp>
2021-11-24 12:40:48 +0900

    poolしてから二乗誤差計算

--- AI/myloss.py
+++ AI/myloss.py
@@ -5,12 +5,23 @@ import torch
 import torch.nn as nn
 
 class MyLoss(nn.Module):
-    def __init__(self):
+    def __init__(self, kernel_size=2, stride=None):
         super().__init__()
 
-    def forward(self, outputs, targets, mask):
-        outputs = outputs * mask
-        targets = targets * mask
+        self.maxpool = nn.MaxPool2d(kernel_size, stride=stride)
+        self.avgpool = nn.AvgPool2d(kernel_size, stride=stride)
 
-        loss = ((outputs - targets) * (outputs - targets)).mean(dim=(0,1,2,3))
-        return loss
+
+    def forward(self, outputs, targets, mask=None, weight=(0.5, 0.5)):
+        if mask != None:
+            outputs = outputs * mask
+            targets = targets * mask
+            N = mask.sum(dim=(0,1,2,3))
+        else:
+            N = 4096
+
+        loss_maxpool = ((maxpool(outputs) - maxpool(targets)).pow(2)).sum(dim=(0,1,2,3))
+        loss_avgpool = ((avgpool(outputs) - avgpool(targets)).pow(2)).sum(dim=(0,1,2,3))
+        #loss = ((outputs - targets) * (outputs - targets)).mean(dim=(0,1,2,3))
+        loss = weight[0] * loss_maxpool + weight[1] * loss_avgpool
+        return loss / N
