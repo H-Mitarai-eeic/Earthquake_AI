f65500263e18d6b113d8064d469e53af948d71cf
H-Mitarai-eeic <mitarai-haruki520@g.ecc.u-tokyo.ac.jp>
2021-11-28 09:28:14 +0900

    コメント追加

--- AI/myloss.py
+++ AI/myloss.py
@@ -7,12 +7,22 @@ import torch.nn as nn
 class MyLoss(nn.Module):
     def __init__(self, kernel_size=2, stride=None):
         super().__init__()
+        """
+        pooling層のkernel_sizeとstrideは、最初に指定してください。
+        """
 
         self.maxpool = nn.MaxPool2d(kernel_size, stride=stride)
         self.avgpool = nn.AvgPool2d(kernel_size, stride=stride)
 
 
     def forward(self, outputs, targets, mask=None, weight=(0.5, 0.5)):
+        """
+        =======引数=======
+        outputs: 予測結果のテンソル
+        targets: 実際のデータのテンソル
+        mask: 観測地点のあるところを 1 、ないところを 0 としたマスク。テンソル。サイズはoutputsやtargetsと同じにすること
+        weight: maxpoolして誤差とるのと, avgpoolして誤差とるのの重みづけのタプル。weight[0] がmaxの重み, weight[1] がavgの重み
+        """
         if mask != None:
             outputs = outputs * mask
             targets = targets * mask
