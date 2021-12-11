127019a41d2d8e10378e1c10bbaca0856551d31a
H-Mitarai-eeic <mitarai-haruki520@g.ecc.u-tokyo.ac.jp>
2021-12-11 12:52:49 +0900

    DNN

--- AI/myfcn.py
+++ AI/myfcn.py
@@ -7,23 +7,16 @@ class MYFCN(nn.Module):
     def __init__(self, in_channels=1, mesh_size=(64, 64, 64)):
         super(MYFCN, self).__init__()
         self.mesh_size = mesh_size
-        """
-        self.conv1 = nn.Conv3d(in_channels, 1, 5, stride=1,padding=2,bias=False)
-        self.relu1 = nn.ReLU(inplace=True)
 
-        self.conv2 = nn.Conv3d(1, 1, 5, stride=1,padding=2,bias=False)
-        self.relu2 = nn.ReLU(inplace=True)
+        self.fc0 = nn.Linear(mesh_size[0] * mesh_size[1] * mesh_size[2], mesh_size[0]/2 * mesh_size[1]/2 * mesh_size[2]/2, bias=False)
 
-        self.conv3 = nn.Conv3d(1, 1, 5, stride=1,padding=2,bias=False)
-        self.relu3 = nn.ReLU(inplace=True)
-        
-        self.conv4 = nn.Conv3d(1, 1, 5, stride=1,padding=2,bias=False)
-        self.relu4 = nn.ReLU(inplace=True)
-        
-        self.conv5 = nn.Conv3d(1, 1, 5, stride=1,padding=2,bias=False)
-        self.relu5 = nn.ReLU(inplace=True)
-        """
-        self.fc0 = nn.Linear(mesh_size[0] * mesh_size[1] * mesh_size[2], 64**2, bias=False)
+        self.fc1 = nn.Linear(mesh_size[0]/2 * mesh_size[1]/2 * mesh_size[2]/2, mesh_size[0]/4 * mesh_size[1]/4 * mesh_size[2]/4, bias=False)
+
+        self.fc2 = nn.Linear(mesh_size[0]/4 * mesh_size[1]/4 * mesh_size[2]/4, mesh_size[0]/4 * mesh_size[1]/4 * 3, bias=False)
+
+        self.fc3 = nn.Linear(mesh_size[0]/4 * mesh_size[1]/4 * 3, mesh_size[0]/2 * mesh_size[1]/2 * 2, bias=False)
+
+        self.fc4 = nn.Linear(mesh_size[0]/2 * mesh_size[1]/2 * 2, 64**2, bias=False)
         #self.relu0 = nn.ReLU6(inplace=True)
 
 
@@ -31,16 +24,14 @@ class MYFCN(nn.Module):
         #print("x:",x.size())
         
         h = x
-        """
-        h = self.relu1(self.conv1(h))
-        h = self.relu2(self.conv2(h))
-        h = self.relu3(self.conv3(h))
-        h = self.relu4(self.conv4(h))
-        h = self.relu5(self.conv5(h))
-        """
         h = h.view(-1, self.mesh_size[0] * self.mesh_size[1] * self.mesh_size[2])
         
         h = self.fc0(h)
+        h = self.fc1(h)
+        h = self.fc2(h)
+        h = self.fc3(h)
+        h = self.fc4(h)
+
         h = h.view(len(x), self.mesh_size[0], self.mesh_size[1])
 
         return h
