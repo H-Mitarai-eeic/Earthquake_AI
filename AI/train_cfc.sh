90635814f7929c0dccd8b4a67a4ccd740f8af9ec
H-Mitarai-eeic <mitarai-haruki520@g.ecc.u-tokyo.ac.jp>
2021-12-21 11:52:47 +0900

    644690

--- AI/train_cfc.sh
+++ AI/train_cfc.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-#SBATCH -p v
+#SBATCH -p p
 #SBATCH --gres=gpu:1
 export PATH=/usr/local/cuda/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 
-python3 train_eq_cfc.py -g 0 -d data1000_honshu6464_mag50_InstrumentalIntensity/ -o result_cfc2/ -b 100 -e 100
+python3 train_eq_cfc.py -g 0 -d data1000_honshu6464_mag50_InstrumentalIntensity/ -o result_cfc3/ -b 100 -e 100
 
 #644688
 #644689
+#644690
