d4675d346a523839407e119364a85aabf43c850b
H-Mitarai-eeic <mitarai-haruki520@g.ecc.u-tokyo.ac.jp>
2021-12-23 14:36:47 +0900

    645043 bias=true

--- AI/mycfc.py
+++ AI/mycfc.py
@@ -8,9 +8,9 @@ class MYFCN(nn.Module):
         super(MYFCN, self).__init__()
         self.mesh_size = mesh_size
 
-        self.conv0 = nn.Conv3d(in_channels, 1, kernel_size=(mesh_size[2]+1, mesh_size[1]+1, mesh_size[0]+1), padding=(int(mesh_size[2]/2), int(mesh_size[1]/2), int(mesh_size[0]/2)), bias=False)
+        self.conv0 = nn.Conv3d(in_channels, 1, kernel_size=(mesh_size[2]+1, mesh_size[1]+1, mesh_size[0]+1), padding=(int(mesh_size[2]/2), int(mesh_size[1]/2), int(mesh_size[0]/2)), bias=True)
 
-        self.fc0 = nn.Linear(mesh_size[0] * mesh_size[1] * mesh_size[2], 64*64, bias=False)
+        self.fc0 = nn.Linear(mesh_size[0] * mesh_size[1] * mesh_size[2], 64*64, bias=True)
 
         #self.fc1 = nn.Linear(256, 64**2, bias=True)
         #self.relu0 = nn.ReLU6(inplace=True)
--- AI/train_cfc.sh
+++ AI/train_cfc.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-#SBATCH -p v
+#SBATCH -p p
 #SBATCH --gres=gpu:1
 export PATH=/usr/local/cuda/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 
-python3 train_eq_cfc.py -g 0 -d data1000_honshu6464_mag50_InstrumentalIntensity/ -o result_cfc4/ -b 100 -e 100
+python3 train_eq_cfc.py -g 0 -d data1000_honshu6464_mag50_InstrumentalIntensity/ -o result_cfc5/ -b 100 -e 100
 
     #644688 cancel
     #644689 cancel
@@ -18,3 +18,4 @@ python3 train_eq_cfc.py -g 0 -d data1000_honshu6464_mag50_InstrumentalIntensity/
 #644765
 # 644811　→ 644812 data all 4乗誤差
 #644821 mylos3
+#645042 →　645043 mylos3 bias true
