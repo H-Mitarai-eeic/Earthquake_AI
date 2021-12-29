0029368492251f5c6f97653c06928ee6c4b8a779
H-Mitarai-eeic <mitarai-haruki520@g.ecc.u-tokyo.ac.jp>
2021-12-29 20:53:08 +0900

    645620 ~ 645625

--- AI/train_cfc2D.sh
+++ AI/train_cfc2D.sh
@@ -4,6 +4,10 @@
 export PATH=/usr/local/cuda/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 
-python3 train_eq_cfc2D.py -g 0 -d data2000_honshu6464_InstrumentalIntensity/ -o result_cfc2D_1/ -b 100 -e 100
+python3 train_eq_cfc2D.py -g 0 -d data2000_honshu6464_InstrumentalIntensity/ -o result_cfc2D_5/ -b 100 -e 100
 
-#645545 data2000 myloss3 
+#645545→645620 data2000 myloss3 
+#645621 data500
+#645623 data2000 exponent = 3
+#645624 data2000 exponent = 1 weight = (0.55, 0.45)
+#645624 data2000 exponent = 1 weight = (0.7, 0.3)
