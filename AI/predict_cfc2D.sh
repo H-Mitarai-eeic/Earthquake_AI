05e8db763f2e2f2e53dfc7494ec04c0af0146f65
H-Mitarai-eeic <mitarai-haruki520@g.ecc.u-tokyo.ac.jp>
2021-12-31 15:36:35 +0900

    ネットワーク修正

---
+++ AI/predict_cfc2D.sh
@@ -0,0 +1,2 @@
+python3 predict_eq_cfc.py -x 52 -y 32 -d 20 -mag 9 -m result_cfc2D_6/model_100
+cat predicted_data_.csv | grep 0
--- AI/train_cfc2D.sh
+++ AI/train_cfc2D.sh
@@ -4,7 +4,7 @@
 export PATH=/usr/local/cuda/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 
-python3 train_eq_cfc2D.py -g 0 -d data2000_honshu6464_InstrumentalIntensity/ -o result_cfc2D_6/ -b 100 -e 100
+python3 train_eq_cfc2D.py -g 0 -d data2000_honshu6464_InstrumentalIntensity/ -o result_cfc2D_7/ -b 100 -e 100
 
 #645545→645620 data2000 myloss3 
 #645621 data500
@@ -12,3 +12,6 @@ python3 train_eq_cfc2D.py -g 0 -d data2000_honshu6464_InstrumentalIntensity/ -o 
 #645624 data2000 exponent = 1 weight = (0.55, 0.45)
 #645625 data2000 exponent = 1 weight = (0.7, 0.3)
 #645626 data2000 exponent = 1 weight = (0.51, 0.49) ===== mask変更 =========
+
+#ネットワーク修正
+#646306 data2000 exponent = 1 weight = (0.51, 0.49) ===== mask変更 =========
