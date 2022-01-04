cfb49fc3a22d1172779f8374e4d3314636f7b370
H-Mitarai-eeic <mitarai-haruki520@g.ecc.u-tokyo.ac.jp>
2022-01-04 15:28:27 +0900

    データの入れ方修正

--- AI/predict_eq_mlp.py
+++ AI/predict_eq_mlp.py
@@ -31,6 +31,7 @@ def main():
 	mesh_size = (64, 64)
 	data_channels = 2
 	depth_max = 600
+	expand = 10
 	net = MYFCN(in_channels=data_channels, mesh_size=mesh_size)
 	# Load designated network weight
 	print("loading Model...")
@@ -45,8 +46,12 @@ def main():
 	mag = float(args.mag)
 
 	epicenter = torch.zeros(1, data_channels, mesh_size[1], mesh_size[0])
-	epicenter[0][0][y][x] = mag / 9
-	epicenter[0][1][y][x] = depth / depth_max
+
+	for Y in range(y - expand, y + expand + 1):
+		for X in range(x - expand, x + expand + 1):
+			if X >= 0 and X < mesh_size[0] and Y >= 0 and Y < mesh_size[1]:
+				epicenter[0][0][Y][X] = mag / 9
+				epicenter[0][1][Y][X] = depth / depth_max
 
 	# Forward
 	outputs = net(epicenter)
