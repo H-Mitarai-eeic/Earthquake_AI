6bcfa486d86300a7008a8be6bda821aa2f1f551e
metaphy-hub <mztnkoyu@g.ecc.u-tokyo.ac.jp>
2022-01-04 15:12:49 +0900

    linearを16*16から64*64に戻した

--- AI/Linear.py
+++ AI/Linear.py
@@ -38,10 +38,8 @@ class Linear(nn.Module):
 
     def __init__(self, n_class=21):
         super(Linear, self).__init__()
-        self.pool1 = nn.MaxPool2d(2, stride=2, ceil_mode=True)
-        self.fc = nn.Linear(int(2*len_data*len_data/16), n_class*len_data*len_data)
-        self.score_fr = nn.Conv2d(8, n_class, 1)
-        self.upscore = nn.ConvTranspose2d(n_class, n_class, 4, stride=4, bias=False) 
+        # self.pool1 = nn.MaxPool2d(2, stride=2, ceil_mode=True)
+        self.linear = nn.Linear(2*len_data*len_data, n_class*len_data*len_data)
 
         self._initialize_weights()
 
@@ -60,9 +58,10 @@ class Linear(nn.Module):
     def forward(self, x):
         h = x
         batch_size = len(x)
-        h = self.pool1(h)
-        h = self.pool1(h)
-        h = h.view(-1, int(2*len_data*len_data/16))
-        h = self.fc(h)
+        # h = self.pool1(h)
+        # h = self.pool1(h)
+        # h = h.view(-1, int(2*len_data*len_data/16))
+        h = h.view(-1, 2*len_data*len_data)
+        h = self.linear(h)
         h = h.view(batch_size, 10, len_data, len_data)
         return h
