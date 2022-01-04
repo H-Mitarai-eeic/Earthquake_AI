c9ce4ea8b63e648667e8f8321958a06ad881b4fa
H-Mitarai-eeic <mitarai-haruki520@g.ecc.u-tokyo.ac.jp>
2022-01-04 16:31:49 +0900

    マグニチュード2乗　650667

--- AI/train_cfc2D.sh
+++ AI/train_cfc2D.sh
@@ -4,7 +4,7 @@
 export PATH=/usr/local/cuda/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 
-python3 train_eq_cfc2D.py -g 0 -d data2000_honshu6464_InstrumentalIntensity/ -o result_cfc2D_11/ -b 100 -e 100
+python3 train_eq_cfc2D.py -g 0 -d data2000_honshu6464_InstrumentalIntensity/ -o result_cfc2D_12/ -b 100 -e 100
 
 #645545→645620 data2000 myloss3 
 #645621 data500
@@ -19,3 +19,5 @@ python3 train_eq_cfc2D.py -g 0 -d data2000_honshu6464_InstrumentalIntensity/ -o 
 #646331 data2000 exponent = 1 weight = (0.8, 0.2) ===== mask変更 =========
 #646334 data2000 exponent = 2 weight = (1, 0) ===== mask変更 =========
 #646347 data2000 exponent = 3 weight = (0.8, 0.2) ===== mask変更 =========
+
+#650667 data2000 exponent = 2 weight = (1, 0) ===== mask変更 ========= マグニチュード2乗
