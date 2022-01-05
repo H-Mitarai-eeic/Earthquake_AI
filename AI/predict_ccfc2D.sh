4961829d18435ec3eb97d642d6aeada64629602e
H-Mitarai-eeic <mitarai-haruki520@g.ecc.u-tokyo.ac.jp>
2022-01-05 09:43:59 +0900

    ブランチ移動

--- AI/predict_ccfc2D.sh
+++ AI/predict_ccfc2D.sh
@@ -1,2 +1,2 @@
-python3 predict_eq_ccfc.py -x 52 -y 32 -d 20 -mag 7 -m result_ccfc2D_11/model_100
+python3 predict_eq_ccfc.py -x 52 -y 32 -d 20 -mag 7 -m result_ccfc2D_1/model_100
 cat predicted_data_.csv | grep 0
