e8b2dd1c1f83ef5b50c6b9be1e5e37925441d2ce
H-Mitarai-eeic <mitarai-haruki520@g.ecc.u-tokyo.ac.jp>
2022-01-14 11:55:14 +0900

    動作検証

--- AI/predict_polycfc2D.sh
+++ AI/predict_polycfc2D.sh
@@ -1,2 +1,1 @@
-python3 predict_eq_polycfc.py -x 52 -y 32 -d 20 -mag 7 -m result_polycfc2D_1/model_17
-cat predicted_data_.csv | grep 0
+python3 predict_eq_polycfc.py -x 52 -y 32 -d 20 -mag 7 -m model_38
