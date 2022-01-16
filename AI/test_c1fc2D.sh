c038903a87b6d9b2ae2fbf1e4b588e4f7def7897
H-Mitarai-eeic <mitarai-haruki520@g.ecc.u-tokyo.ac.jp>
2022-01-16 12:54:02 +0900

    mapmaskのテスト完了

--- AI/test_c1fc2D.sh
+++ AI/test_c1fc2D.sh
@@ -14,7 +14,8 @@ FOR_END=64 #64
 for i in `seq ${FOR_START} ${FOR_END}`
 do
     KERNEL_SIZE=`expr 2 \* ${i} + 1`
-    MODELROOT="result_c1fc2D_karnel_""${KERNEL_SIZE}""/"
+    #MODELROOT="result_c1fc2D_karnel_${KERNEL_SIZE}/"
+    MODELROOT="result_c1fc2D_mapmask_karnel_${KERNEL_SIZE}/"
     MODEL="/model_100"
 
     OUTROOT=${MODELROOT}
@@ -26,8 +27,9 @@ do
     echo "${DATA}"
     echo "KERNEL = ""${KERNEL_SIZE}"
 
-    python3 test_eq_c1fc2D.py -g ${GPU} -b ${MINIBATCH} -d ${DATA} -m ${MODELROOT}${MODEL} -o ${OUTROOT} -kernel_size ${KERNEL_SIZE} >> Stat_data_for_hokkaido.csv
+    python3 test_eq_c1fc2D.py -g ${GPU} -b ${MINIBATCH} -d ${DATA} -m ${MODELROOT}${MODEL} -o ${OUTROOT} -kernel_size ${KERNEL_SIZE} >> Stat_data_for_hokkaido_mapmask.csv
 done
 
 #654325 FOR_START=0 FOR_END=64 kernel_size 1~129
 #654326
+#655024 FOR_START=0 FOR_END=64 kernel_size 1~129 mapmask
