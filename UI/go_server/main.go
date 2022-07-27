789304771b355241b090216831b139b4d150d739
Kakeru Miyazaki <71646518+Kakeru-Miyazaki@users.noreply.github.com>
2022-07-27 13:24:46 +0900

    go server OK

---
+++ UI/go_server/main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os/exec"
+	"strconv"
+)
+
+type Query struct {
+	x     int
+	y     int
+	depth int
+	mag   float64
+}
+
+func query_to_string(q Query) map[string]string {
+	x := strconv.Itoa(q.x)
+	y := strconv.Itoa(q.y)
+	depth := strconv.Itoa(q.depth)
+	mag := strconv.FormatFloat(q.mag, 'f', -1, 64)
+	return map[string]string{"x": x, "y": y, "depth": depth, "mag": mag}
+}
+
+func main() {
+	const PORT = "3000"
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":"+PORT, nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	var err error
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	fmt.Printf("%s %s %s\n", r.Method, r.URL, r.Proto)
+
+	v := r.URL.Query()
+	if v == nil {
+		return
+	}
+
+	var query Query
+	// [MEMO]:
+	// 		check whether the queries are legal value
+	//		by converting there type
+	query.x, err = strconv.Atoi(v["x"][0])
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	query.y, err = strconv.Atoi(v["y"][0])
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	query.depth, err = strconv.Atoi(v["depth"][0])
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	query.mag, err = strconv.ParseFloat(v["mag"][0], 64)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// q_st has the query values as string
+	q_st := query_to_string(query)
+	fmt.Println(q_st)
+	err = exec.Command("python3", "python/Hybrid.py", "-x", q_st["x"], "-y", q_st["y"], "-depth", q_st["depth"], "-mag", q_st["mag"]).Run()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	data, err := ioutil.ReadFile("python/predicted_data.csv")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("prediction ended")
+	fmt.Println()
+
+	// [BE CAREFUL]:
+	// 		This header is set for debug.
+	// 		If you want to delete this header,
+	// 		you have to change the front-end code.
+	header := q_st["x"] + "+" + q_st["y"] + "+" + q_st["depth"] + "+" + q_st["mag"] + ","
+
+	w.Write([]byte(header + string(data)))
+
+}
---
+++ UI/go_server/python/mycfc2D.py
@@ -0,0 +1,32 @@
+import numpy as np
+import torch
+import torch.nn as nn
+
+
+class MYFCN(nn.Module):
+    def __init__(self, in_channels=2, mesh_size=(64, 64), kernel_size=125):
+        super(MYFCN, self).__init__()
+        self.mesh_size = mesh_size
+        self.in_channels = in_channels
+        self.kernel_size=kernel_size
+        #self.conv0 = nn.Conv2d(in_channels, in_channels, kernel_size=(mesh_size[1]+1, mesh_size[0]+1), padding=(int(mesh_size[1]/2), int(mesh_size[0]/2)), bias=False)
+        self.conv0 = nn.Conv2d(in_channels, in_channels, kernel_size=kernel_size, padding=int((kernel_size - 1)/2), bias=False)
+        self.relu0 = nn.ReLU(inplace=True)
+
+        self.fc0 = nn.Linear(mesh_size[0] * mesh_size[1] * in_channels, 64*64, bias=False)
+
+
+    def forward(self, x):
+        #print("x:",x.size())
+        
+        h = x
+
+        h = self.conv0(h)
+        #h = self.relu0(self.conv0(h))
+
+        h = h.view(-1, self.mesh_size[0] * self.mesh_size[1] * self.in_channels)
+        h = self.fc0(h)
+
+        h = h.view(len(x), self.mesh_size[1], self.mesh_size[0])
+
+        return h
