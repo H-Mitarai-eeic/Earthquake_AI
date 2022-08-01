12aba564341559e95e154b48e46739d9a094b19e
Kakeru Miyazaki <71646518+Kakeru-Miyazaki@users.noreply.github.com>
2022-08-01 09:11:01 +0900

    言語変更OK

--- UI/earthquaker/pages/index.tsx
+++ UI/earthquaker/pages/index.tsx
@@ -1,16 +1,24 @@
-import { Typography, useTheme } from "@mui/material";
+import { Box, Stack, styled, Typography, useTheme } from "@mui/material";
 import type { NextPage } from "next";
 import { useState } from "react";
 import Earthquaker from "../components/Earthquaker";
 import English from "../components/Earthquaker";
 import Layout from "../components/Layout";
-import styles from "../styles/Home.module.css";
+import SelectLanguage from "../components/SelectLanguage";
+import { Lang } from "../components/shared/global";
 
 const Home: NextPage = () => {
-    const [language, setLanguage] = useState("English");
+    const [language, setLanguage] = useState<Lang>("English");
+    const theme = useTheme();
     return (
         <Layout title={"Earthquaker"}>
-            <Earthquaker language={"English"}></Earthquaker>
+            <Stack alignItems={"flex-end"} marginBottom={theme.spacing(2)}>
+                <SelectLanguage
+                    language={language}
+                    setLanguage={setLanguage}
+                ></SelectLanguage>
+            </Stack>
+            <Earthquaker language={language}></Earthquaker>
         </Layout>
     );
 };
